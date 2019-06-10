ac73b697d1b0dc83c8cdc8da07c28de36cd34b78
Svet <stsupryk@gmail.com>
2019-06-10 09:52:18 +0300

    Updates

---
+++ precache-manifest.71da948c20ebbfd771cb6c7c6f87e216.js
@@ -0,0 +1,274 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "340a2a7daf7f3628ac29c59d16837599",
+    "url": "/ReactApp/index.html"
+  },
+  {
+    "revision": "be0133f2b2d553c75977",
+    "url": "/ReactApp/static/css/main.37953978.chunk.css"
+  },
+  {
+    "revision": "7263867a213176423749",
+    "url": "/ReactApp/static/js/2.b45d9970.chunk.js"
+  },
+  {
+    "revision": "be0133f2b2d553c75977",
+    "url": "/ReactApp/static/js/main.6c111833.chunk.js"
+  },
+  {
+    "revision": "8fa6fbb6c0e8399fe15f",
+    "url": "/ReactApp/static/js/runtime~main.eb98a27a.js"
+  },
+  {
+    "revision": "7109144d59c384d686497836b0f49777",
+    "url": "/ReactApp/static/media/dell-streak-7.0.7109144d.jpg"
+  },
+  {
+    "revision": "99091431705a9db1f6c5b10db474b221",
+    "url": "/ReactApp/static/media/dell-streak-7.1.99091431.jpg"
+  },
+  {
+    "revision": "4cd57708d5cf4add1ff519ab8d4a2956",
+    "url": "/ReactApp/static/media/dell-streak-7.3.4cd57708.jpg"
+  },
+  {
+    "revision": "897cd18e34655563e51fc5fdffe82dbd",
+    "url": "/ReactApp/static/media/dell-streak-7.4.897cd18e.jpg"
+  },
+  {
+    "revision": "c969e2a99937b20791ab7fb382991cd0",
+    "url": "/ReactApp/static/media/dell-venue.0.c969e2a9.jpg"
+  },
+  {
+    "revision": "bd65baee2072cb387eab40942427f634",
+    "url": "/ReactApp/static/media/dell-venue.2.bd65baee.jpg"
+  },
+  {
+    "revision": "9dfae88f70c1e640fdb504d717238664",
+    "url": "/ReactApp/static/media/dell-venue.3.9dfae88f.jpg"
+  },
+  {
+    "revision": "d4be238403d0b85db97e74e3f7371533",
+    "url": "/ReactApp/static/media/droid-2-global-by-motorola.0.d4be2384.jpg"
+  },
+  {
+    "revision": "d0574a4a5c6e480000624c5324e92255",
+    "url": "/ReactApp/static/media/droid-2-global-by-motorola.1.d0574a4a.jpg"
+  },
+  {
+    "revision": "6da478381887845965a6bc7ec8419edb",
+    "url": "/ReactApp/static/media/droid-2-global-by-motorola.2.6da47838.jpg"
+  },
+  {
+    "revision": "67202f41acbb2b8dcdeeba26090c2b92",
+    "url": "/ReactApp/static/media/droid-pro-by-motorola.0.67202f41.jpg"
+  },
+  {
+    "revision": "21f965a3aeb12b6792c33f6d2c546292",
+    "url": "/ReactApp/static/media/droid-pro-by-motorola.1.21f965a3.jpg"
+  },
+  {
+    "revision": "2e1409a2dd142828d9a2af6b859a3ad5",
+    "url": "/ReactApp/static/media/glyphicons-halflings.2e1409a2.png"
+  },
+  {
+    "revision": "de4e900889194c43056d5a4f6b9ce9d1",
+    "url": "/ReactApp/static/media/lg-axis.0.de4e9008.jpg"
+  },
+  {
+    "revision": "b69427b9a141d5f3b2b30d3ca32099da",
+    "url": "/ReactApp/static/media/lg-axis.1.b69427b9.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactApp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f1619b627aabcc6dcab0df8bead6c135",
+    "url": "/ReactApp/static/media/motorola-atrix-4g.0.f1619b62.jpg"
+  },
+  {
+    "revision": "1e587b1edb318164cfbabc8338d7f652",
+    "url": "/ReactApp/static/media/motorola-atrix-4g.1.1e587b1e.jpg"
+  },
+  {
+    "revision": "97dd0318b9e51075bac45ca3a02146d0",
+    "url": "/ReactApp/static/media/motorola-atrix-4g.2.97dd0318.jpg"
+  },
+  {
+    "revision": "41ff7cb368878b2331b00e92b51f9247",
+    "url": "/ReactApp/static/media/motorola-atrix-4g.3.41ff7cb3.jpg"
+  },
+  {
+    "revision": "f5f8689c5184867335c25f55a0da2dfb",
+    "url": "/ReactApp/static/media/motorola-bravo-with-motoblur.0.f5f8689c.jpg"
+  },
+  {
+    "revision": "68e15b6f3def6d4df7663564ab9339d6",
+    "url": "/ReactApp/static/media/motorola-bravo-with-motoblur.1.68e15b6f.jpg"
+  },
+  {
+    "revision": "f27d677e5ed9f87f75cfe1fb8dea1cef",
+    "url": "/ReactApp/static/media/motorola-bravo-with-motoblur.2.f27d677e.jpg"
+  },
+  {
+    "revision": "70a73ad59529409ad43a783d3ad54701",
+    "url": "/ReactApp/static/media/motorola-charm-with-motoblur.0.70a73ad5.jpg"
+  },
+  {
+    "revision": "3ef2b630673dd9476863a1b695119b94",
+    "url": "/ReactApp/static/media/motorola-charm-with-motoblur.1.3ef2b630.jpg"
+  },
+  {
+    "revision": "055174b41071dcf4a58a989d5608c0da",
+    "url": "/ReactApp/static/media/motorola-charm-with-motoblur.2.055174b4.jpg"
+  },
+  {
+    "revision": "24c5c3152b4004f07f8aa2f13db52876",
+    "url": "/ReactApp/static/media/motorola-defy-with-motoblur.0.24c5c315.jpg"
+  },
+  {
+    "revision": "98fd6e44d2dea08be705c835c9d12fb8",
+    "url": "/ReactApp/static/media/motorola-defy-with-motoblur.1.98fd6e44.jpg"
+  },
+  {
+    "revision": "af937bbf54a70291dbdb17fc5fb5c8b2",
+    "url": "/ReactApp/static/media/motorola-defy-with-motoblur.2.af937bbf.jpg"
+  },
+  {
+    "revision": "850f77805f7d0a43c2cb1034a65a269a",
+    "url": "/ReactApp/static/media/motorola-xoom-with-wi-fi.0.850f7780.jpg"
+  },
+  {
+    "revision": "23f559bf8cc778bbec6257e195b0a592",
+    "url": "/ReactApp/static/media/motorola-xoom-with-wi-fi.1.23f559bf.jpg"
+  },
+  {
+    "revision": "e77544c0053cd4459eb100f25f1d6698",
+    "url": "/ReactApp/static/media/motorola-xoom-with-wi-fi.3.e77544c0.jpg"
+  },
+  {
+    "revision": "a2087de76bde4a5386aa0936e25ac6d8",
+    "url": "/ReactApp/static/media/motorola-xoom-with-wi-fi.4.a2087de7.jpg"
+  },
+  {
+    "revision": "0bfb215c59d717230c5f532ff2ddc2dc",
+    "url": "/ReactApp/static/media/motorola-xoom-with-wi-fi.5.0bfb215c.jpg"
+  },
+  {
+    "revision": "a1295238d0af2cf17bfa6bb96dbb743f",
+    "url": "/ReactApp/static/media/motorola-xoom.0.a1295238.jpg"
+  },
+  {
+    "revision": "b32940150c3793244dafd588251374d8",
+    "url": "/ReactApp/static/media/motorola-xoom.1.b3294015.jpg"
+  },
+  {
+    "revision": "d4ffe892a6ff131d1b565af6ede7bbaf",
+    "url": "/ReactApp/static/media/motorola-xoom.2.d4ffe892.jpg"
+  },
+  {
+    "revision": "19278c8943aea065e9d5035a22dcbab7",
+    "url": "/ReactApp/static/media/nexus-s.0.19278c89.jpg"
+  },
+  {
+    "revision": "9dd391374367cdc3e998d2daf7a18491",
+    "url": "/ReactApp/static/media/nexus-s.1.9dd39137.jpg"
+  },
+  {
+    "revision": "2d5aa71ba2642288f891bcae700e0f09",
+    "url": "/ReactApp/static/media/nexus-s.3.2d5aa71b.jpg"
+  },
+  {
+    "revision": "4653aa7f257fea0d62461054d1ae7794",
+    "url": "/ReactApp/static/media/samsung-galaxy-tab.0.4653aa7f.jpg"
+  },
+  {
+    "revision": "05f224fb31e8ec2e1704a876ce12d6f6",
+    "url": "/ReactApp/static/media/samsung-galaxy-tab.1.05f224fb.jpg"
+  },
+  {
+    "revision": "d1945a076fc31061d3fb2a7b07a49c2a",
+    "url": "/ReactApp/static/media/samsung-galaxy-tab.2.d1945a07.jpg"
+  },
+  {
+    "revision": "efd24a668c3b1b2369ec1f51379b5e70",
+    "url": "/ReactApp/static/media/samsung-galaxy-tab.3.efd24a66.jpg"
+  },
+  {
+    "revision": "15872eead9fdc5f9dbfe474c4c45fe1a",
+    "url": "/ReactApp/static/media/samsung-gem.0.15872eea.jpg"
+  },
+  {
+    "revision": "2ecefa1e260c129e4c8c61012ba16e92",
+    "url": "/ReactApp/static/media/samsung-gem.1.2ecefa1e.jpg"
+  },
+  {
+    "revision": "87f6f319320df4a07a86a5d712f95cf4",
+    "url": "/ReactApp/static/media/samsung-mesmerize-a-galaxy-s-phone.0.87f6f319.jpg"
+  },
+  {
+    "revision": "1eaeda9baa37d4e7d2e4cf38280b0bd8",
+    "url": "/ReactApp/static/media/samsung-mesmerize-a-galaxy-s-phone.1.1eaeda9b.jpg"
+  },
+  {
+    "revision": "87f6f319320df4a07a86a5d712f95cf4",
+    "url": "/ReactApp/static/media/samsung-showcase-a-galaxy-s-phone.0.87f6f319.jpg"
+  },
+  {
+    "revision": "1eaeda9baa37d4e7d2e4cf38280b0bd8",
+    "url": "/ReactApp/static/media/samsung-showcase-a-galaxy-s-phone.1.1eaeda9b.jpg"
+  },
+  {
+    "revision": "5fb1a0bccaf1a3f15ffddd521e7a3611",
+    "url": "/ReactApp/static/media/samsung-transform.0.5fb1a0bc.jpg"
+  },
+  {
+    "revision": "064d924780784dd728a4415a8e1fbd4c",
+    "url": "/ReactApp/static/media/samsung-transform.3.064d9247.jpg"
+  },
+  {
+    "revision": "46194f92ce02c81690090ee47a4eac87",
+    "url": "/ReactApp/static/media/samsung-transform.4.46194f92.jpg"
+  },
+  {
+    "revision": "7464c759947c010cd1538fedf9494622",
+    "url": "/ReactApp/static/media/sanyo-zio.0.7464c759.jpg"
+  },
+  {
+    "revision": "e3dbbf86bd8e745e863f2d44d48150e6",
+    "url": "/ReactApp/static/media/sanyo-zio.1.e3dbbf86.jpg"
+  },
+  {
+    "revision": "6a619950e1350d9ac0c1b3fca3ec43bc",
+    "url": "/ReactApp/static/media/t-mobile-g2.0.6a619950.jpg"
+  },
+  {
+    "revision": "d77dd2b161403269726a4d6e9244b53f",
+    "url": "/ReactApp/static/media/t-mobile-g2.1.d77dd2b1.jpg"
+  },
+  {
+    "revision": "cf3d4d48683bcdc0e0b46cedb684de55",
+    "url": "/ReactApp/static/media/t-mobile-g2.2.cf3d4d48.jpg"
+  },
+  {
+    "revision": "92be065439b25485b6bca02a55d87a5c",
+    "url": "/ReactApp/static/media/t-mobile-mytouch-4g.0.92be0654.jpg"
+  },
+  {
+    "revision": "a8187cd531ca7f71358aab514623ecc9",
+    "url": "/ReactApp/static/media/t-mobile-mytouch-4g.1.a8187cd5.jpg"
+  },
+  {
+    "revision": "8aa57561fedaa596cd6ba28c7c070381",
+    "url": "/ReactApp/static/media/t-mobile-mytouch-4g.2.8aa57561.jpg"
+  },
+  {
+    "revision": "e22576e3cba9a7080faa3e7907f11304",
+    "url": "/ReactApp/static/media/t-mobile-mytouch-4g.3.e22576e3.jpg"
+  },
+  {
+    "revision": "92be065439b25485b6bca02a55d87a5c",
+    "url": "/ReactApp/static/media/t-mobile-mytouch-4g.5.92be0654.jpg"
+  }
+]);
